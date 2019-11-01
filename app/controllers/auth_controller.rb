e1587a2f9ccac6cd21c0bf8fa07a9f829ef01594
kmarks2013 <kmarks2013@gmail.com>
2019-11-01 15:04:19 -0400

    user auth for sign in

---
+++ app/controllers/auth_controller.rb
@@ -0,0 +1,30 @@
+class AuthController < ApplicationController
+
+    def login 
+        user = User.find_by(username: user_params[:username])
+        if user && user.authenticate(user_params[:password])
+            payload = {user_id: user.id}
+            toekn = JWT. encode(payload, hmac_secret, 'HS256')
+            render json: {user: user, token: token}
+        else
+            render json: {errors: user.errors.full_messages}
+        end
+    end
+
+    def persist
+        auth = request.headers['Authorization']
+        if auth
+            token = auth.split(" ")[1]
+            decoded_token = JWT.decode(tokne, hmac_secret, true, {algorithim: "HS256"})
+
+            user = User.find(decoded_token[0]['user_id'])
+            render json: user
+        end
+    end
+
+    private
+    def user_params
+        params.permit(:username, :password)
+    end
+    
+end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,5 @@
 class Role < ApplicationRecord
+    has_many :champion_roles
+    has_many :champions, through: :champion_roles
+    has_many :users, through
 end
