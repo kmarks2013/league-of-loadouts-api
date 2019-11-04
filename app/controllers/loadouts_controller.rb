ad587a0b0c946268e36955b974a96a4e18d17169
kmarks2013 <kmarks2013@gmail.com>
2019-11-03 20:47:08 -0500

    added  crud methods for loadouts

--- app/controllers/loadouts_controller.rb
+++ app/controllers/loadouts_controller.rb
@@ -1,2 +1,39 @@
 class LoadoutsController < ApplicationController
+
+    def index
+        loadouts = Loadout.all
+        render json: loadouts
+    end
+
+    def show
+        loadout = Loadout.find(params[:id) 
+        render json: loadout
+    end
+
+    def create
+        loadout = Loadout.create(loadout_params)
+        if loadout.save?
+            render json: loadout
+        end
+    end
+
+    def update
+        loadout = Loadout.find(params[:id])
+        if loadout.update(loadout_params)
+            render json: loadout
+        end
+    end
+
+    def destroy 
+        loadout = Loadout.find(params[:id])
+        loadout.destroy
+        loadouts = Lodouts.all
+        render json: loadouts
+    end
+
+    private
+
+    def loadout_params
+        params.permit(:user_id, :champion_id, :name)
+    end
 end
