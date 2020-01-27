5b5af21cace6ad65b90828f5ce52d8ce1efc1cff
kmarks2013 <kmarks2013@gmail.com>
2020-01-27 11:15:34 -0500

    suggested a sql order to potentially solve api rendering json out of order

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -2,6 +2,7 @@ class ChampionsController < ApplicationController
     def index
         champions= Champion.all
         render json: champions
+        # try an order function call here to order the json by id. this could solve the api not rendering in order issue.
     end
 
     def show
