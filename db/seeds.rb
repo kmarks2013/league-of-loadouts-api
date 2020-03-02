448aa0aee13573bd9a065936d07f2a8514d3f18f
kmarks2013 <kmarks2013@gmail.com>
2020-03-02 18:40:24 -0500

    comments out user seeds and roles

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,10 +25,10 @@ champions_hash = JSON.parse(champions_json)['data']
 items_hash = JSON.parse(items_json)['data']
 
 # Users
-kenny = User.create(name:"Kenny", password:'1234', username:'kmarks2013', age:31)
-kim = User.create(name:"Kimberly", password:'1', username:'bones', age:22)
-avi = User.create(name:"Avi", password:'1', username:'ivanister', age:25)
-matt = User.create(name:"Matt", password:'1', username:'matt', age:27)
+# kenny = User.create(name:"Kenny", password:'1234', username:'kmarks2013', age:31)
+# kim = User.create(name:"Kimberly", password:'1', username:'bones', age:22)
+# avi = User.create(name:"Avi", password:'1', username:'ivanister', age:25)
+# matt = User.create(name:"Matt", password:'1', username:'matt', age:27)
 
 # Champions
 # for each champion i want to create an instances of the stat model.
@@ -128,12 +128,12 @@ end
 
 
 # Roles
-fighter = Role.create(name:"fighter")
-tank = Role.create(name:"tank")
-mage = Role.create(name:"mage")
-assassin = Role.create(name:"assassin")
-support = Role.create(name:"support")
-marksman = Role.create(name:"marksman")
+# fighter = Role.create(name:"fighter")
+# tank = Role.create(name:"tank")
+# mage = Role.create(name:"mage")
+# assassin = Role.create(name:"assassin")
+# support = Role.create(name:"support")
+# marksman = Role.create(name:"marksman")
 
 
 # stats (chamion 'http://ddragon.leagueoflegends.com/cdn/9.21.1/data/en_US/champion.json')
