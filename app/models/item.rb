b6080f13ac25565a2266e1cbb3c0e167591d3485
kmarks2013 <kmarks2013@gmail.com>
2021-02-23 04:39:35 -0500

    comments out scope in item model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ApplicationRecord
 #   add active record scoping to order the items by name
-    scope :ordered_by_name, -> {order(name: :asc)}
+    # scope :ordered_by_name, -> {order(name: :asc)}
 
 
     def self.default_order
