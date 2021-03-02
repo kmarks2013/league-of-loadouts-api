77c39703d41efee5de118feab8237c667b041d80
kmarks2013 <kmarks2013@gmail.com>
2021-03-01 20:32:48 -0500

    creates valid and invalid user attributes

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-
+    let(:valid_attributes) {
+        "username": 'afegad',
+        "password": "123Ab!",
+        "age": 1,
+        "name": "K"
+    }
+    let(:invalid_attributes){
+        "username": '',
+        "password": "abc",
+        "age": "1",
+    }
 end
