ce457d1e68732eb7d4f5e6d01826626af9a35b80
kmarks2013 <kmarks2013@gmail.com>
2021-03-01 22:15:47 -0500

    updates valid attributes

--- spec/requests/user_request_spec.rb
+++ spec/requests/user_request_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :request do
-    let(:valid_attributes) {
-        "username": 'afegad',
-        "password": "123Ab!",
-        "age": 1,
-        "name": "K"
-    }
-    let(:invalid_attributes){
-        "username": '',
-        "password": "abc",
-        "age": "1"
-    }
+    let(:valid_attributes) do
+        {
+            "username": 'afegad',
+            "password": "123Ab!",
+            "age": 1,
+            "name": "K"
+        }
+    end
+    let(:invalid_attributes) do
+        {
+            "username": '',
+            "password": "abc",
+            "age": "1"
+        }
+    end
 
     describe "GET /index" do
         it 'renders the list of users' do
