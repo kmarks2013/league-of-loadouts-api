85630c886f3817b8d64cebb174a5793cb3169cb1
kmarks2013 <kmarks2013@gmail.com>
2021-03-03 09:06:26 -0500

    removes comments from loadout controller

--- app/controllers/loadouts_controller.rb
+++ app/controllers/loadouts_controller.rb
@@ -16,10 +16,6 @@ class LoadoutsController < ApplicationController
     end
 
     def create
-        # Goal is to make sure a loadout can not be created unless they have the correct token
-        # A loadout should only be made if there is a current user.
-        # A loadout should be assigned the user id of the current user it shouldn't be passed back online.
-
         if current_user
             loadout = current_user.loadouts.new(loadout_params)
             if loadout && loadout.save
@@ -33,18 +29,6 @@ class LoadoutsController < ApplicationController
     end
 
     def update
-        # Goal is to make sure a user can not be updated unless they have the correct token and it belongs the user who created it
-        # A loadout should only be updated if there is a current user.
-        # then the update sould only take place if the loadout out belongs to that current user
-
-        # update should rebuild the loadout object with hte id being params:id, the user_id being current_user id, and the champion_id should be set from the params[champion:id]:
-        # {
-        #  id: params[:id],
-        #  user_id: current_user.id,
-        #  champion_id: params[:champion_id],
-        #  name: loadout_update_params
-        # }
-
         if current_user
             loadout = Loadout.find(params[:id])
             if loadout[:user_id] == current_user.id
@@ -60,22 +44,12 @@ class LoadoutsController < ApplicationController
         else
             render json: {error: "You must be logged in to preform this action."}, status: :unauthorized
         end
-        # if loadout.update(loadout_params)
-        #     render json: loadout
-        # end
-        # byebug
     end
 
     def destroy 
-        # Goal is to make sure a loadout can not be destroyed unless they have the correct token  and it belongs the user who created it
-        # A loadout should only be able to be deleted if there is a current user.
-        # and it should only be destroyed if it bleongs to the current user
-        # byebug
         if current_user
             loadout = Loadout.find(params[:id])
             if loadout[:user_id] == current_user.id
-                # byebug
-                # needed to choose between rendering every loadout or rendering the current_users loadouts
                 loadout.destroy
                 render json: Loadout.all, status: :accepted
             else
