5d95062b5d3be0ca128220e5133d9ac290a8e486
kmarks2013 <kmarks2013@gmail.com>
2021-03-03 09:05:53 -0500

    removes comments from loadout item controlelr

--- app/controllers/loadout_items_controller.rb
+++ app/controllers/loadout_items_controller.rb
@@ -12,11 +12,6 @@ class LoadoutItemsController < ApplicationController
     end
 
     def create
-        # byebug
-        # Goal is to make sure a loadout item  can not be added unless there is a valid auth token and the loadout belongs to that user.
-        # Loadout Item should only be added to a corresponding loadout and when the loadout belongs to the current user.
-        # Loadout id should be added
-        # byebug
         if current_user
             loadout = Loadout.find(loadout_item_params[:loadout_id])
             if loadout.user.id == current_user.id
@@ -24,12 +19,7 @@ class LoadoutItemsController < ApplicationController
                     item = Item.find(item_id.to_i)
                     loadout_item = LoadoutItem.create(loadout_id: loadout_item_params[:loadout_id], item_id: item.id)
                 end
-                # if loadout.items.length
                     render json: loadout
-                # else
-                #     render json: {errors: "Items were not added" }, status: :unprocessable_entity
-                # end
-
             else
                 render json: {error: "Unauthorized Access Restricted"}, status: :unauthorized
             end
@@ -39,13 +29,10 @@ class LoadoutItemsController < ApplicationController
     end
 
     def destroy
-        # Goal is to make sure a loadout item can not be deleted unless there is a valid auth token and the loadout belongs to that user.
-        # conditional layers: current user, loadout ownership loadout item will find its own loadout.
         if current_user
             loadout_item = LoadoutItem.find(params[:id])
             loadout = Loadout.find(loadout_item.loadout_id)
             if loadout.user.id == current_user.id
-                # byebug
                 loadout_item.destroy
                 render json: loadout
             else
