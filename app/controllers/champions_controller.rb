3ccb39cf772594828177f821d3cf8a8717c28f9f
kmarks2013 <kmarks2013@gmail.com>
2021-03-03 09:04:37 -0500

    removes comment from champion controller

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -2,7 +2,6 @@ class ChampionsController < ApplicationController
     def index
         champions= Champion.all
         render json: champions.ordered_by_name
-        # try an order function call here to order the json by id. this could solve the api not rendering in order issue.
     end
 
     def show
