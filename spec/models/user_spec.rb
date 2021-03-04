ea4d3f895ef5153192e9b538c8b4dcd432762628
kmarks2013 <kmarks2013@gmail.com>
2021-03-04 16:54:12 -0500

    removes pending and adds a user and the creation test for the user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    before(:each) do
+        @user = User.create!(name: "Testor", username: "testuser", password:'Testpass1!', age: 23)
+    end
+
+    describe 'creation' do
+        it 'adds a user to the database' do
+            expect(User.all.count).to eq(1)
+        end
+    end
+
 end
