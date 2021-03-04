11e7c0be79296a8491bb7ee399cbe5ab9c3cecfb
kmarks2013 <kmarks2013@gmail.com>
2021-03-04 16:47:42 -0500

    updates strong parameters to require the user object

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,7 +46,7 @@ class UsersController < ApplicationController
 
     private
     def user_params
-        params.permit(:name, :username, :password, :age)
+        params.require(:user).permit([:name, :username, :password, :age])
     end
 
     # def update_params
