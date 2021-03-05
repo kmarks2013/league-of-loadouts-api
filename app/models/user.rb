f4f3b1ecf0fd81dca814df7f4e3daf5d02e53e2a
kmarks2013 <kmarks2013@gmail.com>
2021-03-04 19:23:58 -0500

    comments out password format and password format validation to work on testing the validaiton

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,14 +12,14 @@ class User < ApplicationRecord
     # validates :username, uniqueness: {message: "This username has already been taken"}, case_insensitive: false
     validates :username, :length => { :in => 6..30 }
     
-    PASSWORD_FORMAT = /\A
-        (?=.*[A-Z])
-        (?=.*[a-z])
-        (?=.*\d)
-        (?=.*[[:^alnum:]])
-    /x
+    # PASSWORD_FORMAT = /\A
+    #     (?=.*[A-Z])
+    #     (?=.*[a-z])
+    #     (?=.*\d)
+    #     (?=.*[[:^alnum:]])
+    # /x
     has_secure_password
     validates :password, :length => { :minimum => 6 }
-    validates :password, format: { with: PASSWORD_FORMAT, :message => 'Password must include: 1 uppercase, 1 lowercase, 1 digit and 1 special character' }
+    # validates :password, format: { with: PASSWORD_FORMAT, :message => 'Password must include: 1 uppercase, 1 lowercase, 1 digit and 1 special character' }
 
 end
