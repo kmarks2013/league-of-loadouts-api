68d10f3a4c51667f7ab31a9efdeb83476b5e9514
kmarks2013 <kmarks2013@gmail.com>
2021-03-04 20:48:20 -0500

    deletes unnecessary comments

--- spec/requests/loadouts_spec.rb
+++ spec/requests/loadouts_spec.rb
@@ -24,10 +24,6 @@ RSpec.describe "/loadouts", type: :request do
     skip("Add a hash of attributes invalid for your model")
   }
 
-  # This should return the minimal set of values that should be in the headers
-  # in order to pass any filters (e.g. authentication) defined in
-  # LoadoutsController, or in your router and rack
-  # middleware. Be sure to keep this updated too.
   let(:valid_headers) {
     {}
   }
