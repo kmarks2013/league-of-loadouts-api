494fb0cbb8405eca07c28c9ee905a2dd893e9a81
kmarks2013 <kmarks2013@gmail.com>
2021-03-05 10:06:02 -0500

    adds blocks for valid headers, validattributes and invalid attributes

--- spec/requests/loadout_item_request_spec.rb
+++ spec/requests/loadout_item_request_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe "LoadoutItems", type: :request do
+# neeed 4 specific tests index, show, create destroy
+    let(:valid_attributes) do
+        {
+
+        }
+    end
+    let(:invalid_attributes) do
+        {
+
+        }
+    end
+    let(:valid_headers)do
+        {
+
+        }
+    end
+
 
 end
