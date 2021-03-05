38ff16b9395415fd6d8c25f4f29c97e45e5f26b6
kmarks2013 <kmarks2013@gmail.com>
2021-03-04 19:06:45 -0500

    creates encode token helper methodg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,13 @@
 class ApplicationController < ActionController::API
+    def user_payload(user)
+        { user_id: user.id}
+    end
+
     def hmac_secret
         ENV['JWT_SECRET_KEY']
+    end
+
+    def encode_token(payload)
+        JWT.encode(payload, hmac_secret, 'HS256')
     end
 end
