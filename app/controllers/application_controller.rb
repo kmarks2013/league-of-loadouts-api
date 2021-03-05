f512b56750e079000b66ce8672601b2ae550220c
kmarks2013 <kmarks2013@gmail.com>
2021-03-04 19:15:08 -0500

    creates a current_user method to check when there is a current user or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,20 @@ class ApplicationController < ActionController::API
     def encode_token(payload)
         JWT.encode(payload, hmac_secret, 'HS256')
     end
+
+    def current_user
+        if request.headers['Authorization']
+            token = request.headers["Authorization"]
+            user_token = token.split(' ')[1]
+            begin
+                # user_token
+                decoded_token = JWT.decode(user_token, hmac_secret, true, {algorithim: "HS256"})
+        #         byebug
+                User.find(decoded_token[0]['user_id'])
+            rescue
+                # byebug
+                nil
+            end
+        end
+    end
 end
