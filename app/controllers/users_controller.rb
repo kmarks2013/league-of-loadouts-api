4b1da543e7efa131305cd0c31b610c0ebf9e86c9
kmarks2013 <kmarks2013@gmail.com>
2021-03-10 22:29:04 -0500

    removes comments about finding the current users id and my logic error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,8 +23,6 @@ class UsersController < ApplicationController
 
     def update
         # add a guard cluase to render the error unless there is a current user?
-        # Finding the user using the current user's id because when update fails on the current user the errors comes back as an empty object
-        # this current update funciton will always find the current user and update them it doesn't error out if it tries to update the wrong user. need to fix this
         user = User.find(params[:id])
             # byebug
         if user.id == current_user.id
