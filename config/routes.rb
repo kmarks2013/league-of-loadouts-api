0881a359f257b0dcab6ae4d5d72fe6107db6529a
Kenneth Marks <52679204+kmarks2013@users.noreply.github.com>
2021-03-19 16:40:19 -0400

    Update routes.rb
    Adds default JSON formatting to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,16 @@
 Rails.application.routes.draw do
-  resources :stats
-  resources :champion_roles
-  resources :roles
-  resources :loadout_items,  only: [:index, :show, :create,:destroy]
-  resources :items
-  resources :loadouts
-  resources :champions
-  resources :users
+  defaults format: :json do 
+    resources :stats
+    resources :champion_roles
+    resources :roles
+    resources :loadout_items,  only: [:index, :show, :create,:destroy]
+    resources :items
+    resources :loadouts
+    resources :champions
+    resources :users
 
-  # delete '/loadout_items/', to: 'loadout_items#destroy'
-
-  get '/persist', to: 'auth#persist'
-  post '/login', to: 'auth#login'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+    get '/persist', to: 'auth#persist'
+    post '/login', to: 'auth#login'
+  end
+  
 end
